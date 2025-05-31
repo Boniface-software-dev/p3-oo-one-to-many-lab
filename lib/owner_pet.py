af47cbeaed365c5ab8a1c960ce7f332412fd0c05
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-31 20:59:22 +0300

    class Pet

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,27 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
 
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self.pet_type = pet_type
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an Owner instance")
+        self._owner = owner
+
+        Pet.all.append(self)
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, value):
+        if not isinstance(value, Owner):
+            raise Exception("owner must be an Owner instance")
+        self._owner = value
 class Owner:
     pass
