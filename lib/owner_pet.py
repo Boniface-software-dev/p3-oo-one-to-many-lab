7c9b42d7baa59f490ac8b957728703ace66bdf34
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-05-31 21:10:53 +0300

    passed test

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -23,5 +23,18 @@ class Pet:
         if not isinstance(value, Owner):
             raise Exception("owner must be an Owner instance")
         self._owner = value
-class Owner:
-    pass
+
+class Owner:    
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("add_pet expects a Pet instance")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
